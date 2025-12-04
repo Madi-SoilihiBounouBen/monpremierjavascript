a530598f64260dd491a12f1d00e9fff845c4fa04
Madi-SoilihiBounouBen <madisoilihi1999@gmail.com>
2025-12-04 15:47:02 +0300

    J'ai créé un gestion de biblitheque. Fait des calcul et afficher les résultats

---
+++ gestionBibliotheque.js
@@ -0,0 +1,13 @@
+let totalLivres = 500; // Quantité initiale
+console.log(totalLivres)
+
+totalLivres += 50;  // Achat de 50 livres
+console.log(totalLivres)
+
+totalLivres -= 10;  // On jette 10 livres
+console.log(totalLivres)
+
+totalLivres += 5;   // On rachète 5 livres
+console.log(totalLivres); // Résultat final
+
+
