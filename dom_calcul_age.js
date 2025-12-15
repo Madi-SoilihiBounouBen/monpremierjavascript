60104b1a132cb6117186fa487a08586c96339585
Madi-SoilihiBounouBen <madisoilihi1999@gmail.com>
2025-12-15 16:43:51 +0300

    J'ai des récupérations de données des clients grace à JavaScript sur console

--- dom_calcul_age.js
+++ dom_calcul_age.js
@@ -11,3 +11,28 @@ function calculAge() {
         ageAffichage.textContent = "";
     }
 }
+
+// Récupère l'élément HTML dont l'id est "boutonValider"
+let elementBoutonValider = document.getElementById("boutonValider");
+// Ajoute un écouteur d'événement sur cet élément
+// L'événement écouté est le clic de la souris ("click")
+//addEventListener est une méthode JavaScript qui permet de dire à un élément HTML :
+//« Quand un événement se produit, exécute ce code »
+elementBoutonValider.addEventListener("click", function(){
+    // Affiche dans la console du navigateur le texte
+    // "elementBoutonValider :" suivi de l'élément HTML lui-même
+    console.log("elementBoutonValider :", elementBoutonValider);
+    console.log(elementBoutonValider.clientHeight);
+
+});
+
+let elementInputNom = document.querySelector("#nom");//le # c'est pour indique l'élément séléctionné est id
+    console.log(elementInputNom);
+    console.log(document.head);
+    console.log(document.title);
+
+let elementDivRecensement = document.querySelector("#recensement");
+    console.log(elementDivRecensement);
+
+    let elementPRecensement = document.querySelector("#paragraphe1");
+    console.log(elementPRecensement);
