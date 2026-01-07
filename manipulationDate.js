cbe6c38d2f68581a9d35515977f522982b1d9f23
Madi-SoilihiBounouBen <madisoilihi1999@gmail.com>
2026-01-07 09:40:01 +0300

    J'ai mis un horloge de Tokyo, Mexique et Montréal. J'ai rajouté une image de fond à ma page web

--- manipulationDate.js
+++ manipulationDate.js
@@ -1,7 +1,7 @@
 // Manipulation des dates en JavaScript
 console.log("### Je suis dans l'événement date ###");
 
-const maDate2 = new Date();
+const maDate1 = new Date();
 
 const anneeEnCours = new Date().getFullYear();
 
@@ -9,17 +9,52 @@ let elementSpanAnee = document.querySelector("#annee");
 elementSpanAnee.textContent = anneeEnCours;
 
 let elementSpanMois = document.querySelector("#mois");
-elementSpanMois.textContent = maDate2.getMonth();
+elementSpanMois.textContent = maDate1.getMonth();
 
 let dateComplet = document.getElementById("dateJour");
-dateComplet.textContent = maDate2.toLocaleDateString();
+dateComplet.textContent = maDate1.toLocaleDateString();
 
 let heureLocal = document.getElementById("heureLocal");
-heureLocal.textContent = maDate2.toLocaleTimeString();
+heureLocal.textContent = maDate1.toLocaleTimeString(); //le même code que la function, juste que ça ne s'anime pas
 
 let heureTokyo = document.getElementById("heureParis");
-heureTokyo.textContent = maDate2.toLocaleTimeString("fr-FR", {
+heureTokyo.textContent = maDate1.toLocaleTimeString("fr-FR", {
   timeZone: "Europe/Paris",
- });
+ });//le même code que la function, juste que ça ne s'anime pas
 
 
+function afficherHeures() {
+  const maDate2 = new Date();
+
+  //Heure Mayotte
+  let heureLocal = document.getElementById("heureLocal");
+  heureLocal.textContent = maDate2.toLocaleTimeString();
+
+  //Heure Paris
+  let heureParis = document.getElementById("heureParis");
+  heureParis.textContent = maDate2.toLocaleTimeString("fr-FR", {
+    timeZone: "Europe/Paris",
+  });
+
+  // Heure Tokyo
+  const heureTokyo = document.getElementById("heureTokyo");
+  heureTokyo.textContent = maDate2.toLocaleTimeString("ja-JP", {
+    timeZone: "Asia/Tokyo",
+  });
+
+  const heureMexique =document.getElementById("heureMexique");
+  heureMexique.textContent = maDate2.toLocaleTimeString("es-MX", {
+      timeZone: "America/Mexico_City",
+    });
+
+  const heureMontreal = document.getElementById("heureMontreal");
+  heureMontreal.textContent = maDate2.toLocaleTimeString("fr-CA", {
+      timeZone: "America/Toronto",
+    });
+}
+
+// mise à jour chaque seconde
+setInterval(afficherHeures, 1000);
+
+// affichage immédiat au chargement
+afficherHeures();
